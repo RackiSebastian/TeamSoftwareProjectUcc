b37cc91d9c719337f1c3e89c2ad8ede53aafac3c
RackiSebastian <118459412@umail.ucc.ie>
2021-02-05 11:32:01 +0000

    conflicts>?

--- spotifyApp/frontCode/models.py
+++ spotifyApp/frontCode/models.py
@@ -5,6 +5,8 @@ import random
 def unique_code(): 
 	length = 4
 
+	
+
 	while True: 
 		code = ''.join(random.choices(string.ascii_lowercase, k = lenght))
 		if Room.objects.filter(code = code).count() == 0:
@@ -13,9 +15,11 @@ def unique_code():
 
 
 class Room(models.Model):
-	code = models.CharField(max_length = 7, default = "",unique = True)
+	code = models.CharField(max_length = 7, default = unique_code(),unique = True)
 	host = models.CharField(max_length = 255,unique = True)
 	can_pause = models.BooleanField(null = False,default = False)
 	vote_to_skip = models.IntegerField(null = False,default = 0) 
 	date_time = models.DateTimeField(auto_now_add =True)
-	
+
+
+print("hello world")
