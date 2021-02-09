f89f69e93e7ea34d7e4e173b63e5f12f43dca4ac
Allan Barry <allan1barry@gmail.com>
2021-02-09 20:38:14 +0000

    Fixed webpack and css issues

--- spotifyApp/frontEndReact/src/index.js
+++ spotifyApp/frontEndReact/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-//import '/static/css/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../static/css/index.css';
 //import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 
