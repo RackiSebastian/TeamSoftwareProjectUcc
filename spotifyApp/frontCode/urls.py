aedbcf122bd56b3aa71bbf05b893b7288ab57a0f
RackiSebastian <118459412@umail.ucc.ie>
2021-02-15 13:28:07 +0000

    Pushing new backend code

--- spotifyApp/frontCode/urls.py
+++ spotifyApp/frontCode/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path 
-from .views import RoomView, CreateRoom
+from .views import RoomView, CreateRoom,getRoom,joinRoom
 
 urlpatterns = [
-  
+  path('room',RoomView.as_view()),
+  path('createRoom',CreateRoom.as_view()),
+  path('getRoom',getRoom.as_view()),
+  path('leaveRoom',leaveRoom.as_view()),
+  path('joinRoom',joinRoom.as_view()),
 ]
